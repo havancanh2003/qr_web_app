eabb35fa2896e18aed7b1b6f8db7601b22beae3b
ha van canh <havancanh2003@gmail.com>
2023-06-16 01:22:29 +0700

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+
+export const publicRoutes = [
+  { path: "/", component: Home },
+  { path: "/cart", component: Cart },
+  //{ path: "/upload", component: Upload, layout: null },
+];
