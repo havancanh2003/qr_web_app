24366bbdbac0f51dbcd6cdcfc1929c0e1393afd9
Tranhuy32 <tranxuanhuy3201@gmail.com>
2023-06-24 14:09:12 +0700

    update session storage

--- src/components/DefaultLayout/Header/index.js
+++ src/components/DefaultLayout/Header/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import style from "./Header.module.scss";
 import classNames from "classnames/bind";
-
 import { FaBoxTissue, FaArtstation } from "react-icons/fa";
-
 import { useNavigate } from "react-router-dom";
 
 const cx = classNames.bind(style);
-function click() {
-  //console.log(1);
-}
 
 const Header = () => {
   const navigate = useNavigate();
+
+  const handleClick = () => {
+    const table = sessionStorage.getItem("table");
+    console.log(table);
+    navigate(`/home/${table}`);
+  };
+
   return (
     <header>
       {/* <Sidebar className={cx("sidebar")} /> */}
@@ -20,7 +22,7 @@ const Header = () => {
       <img
         src="https://static.vecteezy.com/system/resources/previews/014/971/638/non_2x/food-logo-design-template-restaurant-free-png.png"
         alt=""
-        onClick={() => navigate("/")}
+        onClick={handleClick}
       />
       {/* <FaBoxTissue className={cx("cart")} /> */}
     </header>
--- src/pages/Home/indexHome.js
+++ src/pages/Home/indexHome.js
@@ -1,18 +1,24 @@
 import * as React from "react";
-import { Routes, Route, useParams, useNavigate, Link } from "react-router-dom";
-import { Fragment } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Fragment, useEffect } from "react";
 import icon from "../../assets/image/icon.png";
 import Headerhome from "../../components/DefaultLayout/Headerhome";
 import classNames from "classnames/bind";
 import style from "./home.scss";
 const cx = classNames.bind(style);
 function Home() {
+  const { table } = useParams();
   function ProfilePage() {
     // Get the userId param from the URL.
-    let { userId } = useParams();
+    // let { table } = useParams();
     // ...
   }
-  const navigate = useNavigate()
+
+  useEffect(() => {
+    sessionStorage.setItem("table", table);
+  }, [table]);
+
+  const navigate = useNavigate();
   return (
     <Fragment>
       <Headerhome />
@@ -22,21 +28,20 @@ function Home() {
           <span>Bạn đang ngồi bàn:</span>
           <hr />
         </p>
-      <button className="homeButton">
-        <img src={icon} alt="icon"></img>
-        <span>Gọi Nhân Viên</span>
-      </button>
-      <button 
-        className="homeButton" 
-        id="secondButton"
-        onClick={() => navigate('Menu')}
+        <button className="homeButton">
+          <img src={icon} alt="icon"></img>
+          <span>Gọi Nhân Viên</span>
+        </button>
+        <button
+          className="homeButton"
+          id="secondButton"
+          onClick={() => navigate('/menu')}
         >
-        Xem Menu - Gọi Món
-      </button>
-      <p id="introduce">Powed by 4Flex</p>
-    </div>
+          Xem Menu - Gọi Món
+        </button>
+        <p id="introduce">Powed by 4Flex</p>
+      </div>
     </Fragment>
-
   );
 }
 
