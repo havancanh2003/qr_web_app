422f024c10ef4edf40cf468d9d01b41482184743
Tranhuy32 <tranxuanhuy3201@gmail.com>
2023-06-25 15:48:58 +0700

    add session dummy data

---
+++ src/data/SessionStorage.js
@@ -0,0 +1,37 @@
+export const sessionStorage = [
+  {
+    id: "6497f3d8207edcf74dd69c78",
+    image_detail: {
+      id: "6497f86bec184b7687b6cf58",
+      path: "http://117.4.194.207:3003/image/c10223ed73bdc6868ada8dfbb710e10b83f.jpg",
+    },
+    name: "Phở vietnamese",
+    price: 35000,
+    options: ["Tái lăn"],
+    number: 1,
+  },
+  //
+  {
+    id: "6497f2c1207edcf74dd69c5a",
+    image_detail: {
+      id: "6497f2c1207edcf74dd69c55",
+      path: "http://localhost:3003/image/7592b70c8c313bd07102d93c645b0f675.jpg",
+    },
+    name: "Bánh mì",
+    price: 15000,
+    options: ["Ít ớt"],
+    number: 1,
+  },
+  //
+  {
+    id: "6497ed4d7cc6565cd8a5135b",
+    image_detail: {
+      id: "6497f90dec184b7687b6cf6e",
+      path: "http://117.4.194.207:3003/image/71ed104eb8445859387fea357f1bda499.jpg",
+    },
+    name: "Coca",
+    price: 15000,
+    options: ["30% đá"],
+    number: "2",
+  },
+];
