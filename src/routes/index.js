6d7eded81c0e370d51f77540cb986070e2be17bd
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2023-06-27 21:19:10 +0700

    fix navbar ui, add navbar active

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ export const publicRoutes = [
   { path: "/home/:table", component: Home, layout: null },
   { path: "/menu", component: Menu },
   { path: "/cart", component: Cart, layout:null },
-  { path: "/showall", component: ShowAll },
+  { path: "/showall", component: ShowAll,layout:null },
   { path: "/detail", component: Detail },
   { path: "*", component: Fragment, layout: null },
   //{ path: "/upload", component: Upload, layout: null },
